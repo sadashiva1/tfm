09b27e35301840fa519a7451e53c2cf7a85a347a
sadashiva1 <sadashiva9933@Gmail.com>
2024-09-29 15:48:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -40,6 +40,38 @@ resource "github_repository_file" "readme" {
   overwrite_on_create = true
 }
 
+provider "github" {
+  token = var.github_token
+}
+
+variable "github_token" {
+  type    = string
+  default = "" # Optional, or use sensitive = true in TF 0.14+
+}
+
+resource "github_repository" "tfm_repo" {
+  name        = "tfm_repo"
+  description = "Terraform managed repo"
+  visibility  = "public"
+  auto_init   = true
+}
+
+resource "github_repository_file" "example_file" {
+  repository    = github_repository.tfm_repo.name  # Use the repo name
+  file          = "example.txt"                     # The file you want to create or update
+  content       = "Hello, World!"                   # Content of the file
+  commit_message = "Update example.txt with Hello, World!"  # Commit message
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "readme" {
+  repository    = github_repository.tfm_repo.name  # Use the repo name
+  branch        = "main"
+  file          = "README.md"                     # The file you want to create or update
+  content       = "ReadMe file created using terraform"                   # Content of the file
+  commit_message = "Update example.txt with Hello, World!"  # Commit message
+  overwrite_on_create = true
+}
 
 
 
@@ -52,8 +84,17 @@ resource "github_repository_file" "readme" {
 # terraform integrations
 # terraform plan
 # terraform apply
+
+#terraform plan -var "github_token=$GITHUB_TOKEN"
+#terraform apply -var "github_token=$GITHUB_TOKEN"
+
 # terraform plan -out=plan1
 # terraform apply plan1
+# terraform show
+# terraform state list
+# terraform state show resource_type.resource_name
+# terraform state show github_repository.tfm_repo
+
 # terraform show
 # terraform state list
 # terraform state show resource_type.resource_name
